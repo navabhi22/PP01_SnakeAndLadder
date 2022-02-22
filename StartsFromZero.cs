dc3ebaa6630da6119d3c90c27f33622576cb09be
navabhi22 <navabhi22@gmail.com>
2022-02-22 16:10:40 +0530

    Updated Class to use in class ExactWin

--- StartsFromZero.cs
+++ StartsFromZero.cs
@@ -10,17 +10,11 @@ namespace PP01SnakeAndLadder
     {
         /// <summary>
         /// Restart at zero if position is less than zero
-        /// returns num of dice roll and poition.
         /// </summary>
         /// <returns></returns>
-        public static int[] RestartAtZero()
+        public static int RestartAtZero(int roll, int Position)
         {
-            int Position = 0;
             int counter = 0;
-            while (Position <= 100)
-
-            {
-                int roll = DiceRoll.DiceRollValue();
                 if (Position <= 0)
                 {
                     Position = 0;
@@ -31,11 +25,8 @@ namespace PP01SnakeAndLadder
                     Position = CheckOption.Option(roll, Position);
                 }
                 counter++;
-            }
-            int[] arr = new int[2];
-            arr[0] = counter;
-            arr[1] = Position;
-            return arr;
+            
+            return Position;
         }
     }
 }
