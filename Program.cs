2b974647f6c2d893f39501d6be0fba4e27f1cd10
navabhi22 <navabhi22@gmail.com>
2022-02-22 11:05:22 +0530

    Greeting Message for Snake And Ladder

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PP01SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This problem simulates a Snake and Ladder Game");      
+        }
+    }
+}
