2942e5e4e30e765a4dfe41611dc464db12bd4d42
navabhi22 <navabhi22@gmail.com>
2022-02-22 16:11:34 +0530

    Added class to get Exact Winning Position 100

---
+++ ExactWin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP01SnakeAndLadder
+{
+    public class ExactWin
+    {
+        /// <summary>
+        /// Get the exact winning pssition.
+        /// </summary>
+        public static void ExactWinningPossition()
+        {
+            int Position = 0;
+            int Counter = 0;
+            while (Position < 100)
+            {
+                
+                int roll = DiceRoll.DiceRollValue();
+                if (Position == 99 && roll == 1)
+                {
+                    Position = StartsFromZero.RestartAtZero(roll, Position);
+                }
+                else if (Position == 98 && roll <= 2)
+                {
+                    Position = StartsFromZero.RestartAtZero(roll, Position);
+
+                }
+                else if (Position == 97 && roll <= 3)
+                {
+                    Position = StartsFromZero.RestartAtZero(roll, Position);
+                }
+                else if (Position == 96 && roll <= 4)
+                {
+                    Position = StartsFromZero.RestartAtZero(roll, Position);
+                }
+                else if (Position == 95 && roll <= 5)
+                {
+                    Position = StartsFromZero.RestartAtZero(roll, Position);
+                }
+                else if (Position < 95)
+                {
+                    Position = StartsFromZero.RestartAtZero(roll, Position);
+                }
+                Counter++;
+            }
+            Console.WriteLine("Players Final Position " + Position);
+        }
+    }
+}
