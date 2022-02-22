16b5aad9d5472fb6594c07e8e3e271eec1a99ae3
navabhi22 <navabhi22@gmail.com>
2022-02-22 17:59:41 +0530

    Updated class for Two Player Game

--- ExactWin.cs
+++ ExactWin.cs
@@ -9,47 +9,43 @@ namespace PP01SnakeAndLadder
     public class ExactWin
     {
         /// <summary>
+        /// Updated for Two Player Game
         /// Get the exact winning pssition and number of dice rolls to reach the position 
         /// </summary>
-        public static void ExactWinningPossition()
+        public static int ExactWinningPossition(int Position, int roll)
         {
-            int Position = 0;
-            int Counter = 0;
-            while (Position < 100)
-            {
-                
-                int roll = DiceRoll.DiceRollValue();
-                Console.WriteLine("Dice roll value is " +roll);
-                if (Position == 99 && roll == 1)
-                {
-                    Position = StartsFromZero.RestartAtZero(roll, Position);
-                }
-                else if (Position == 98 && roll <= 2)
-                {
-                    Position = StartsFromZero.RestartAtZero(roll, Position);
 
-                }
-                else if (Position == 97 && roll <= 3)
-                {
-                    Position = StartsFromZero.RestartAtZero(roll, Position);
-                }
-                else if (Position == 96 && roll <= 4)
-                {
-                    Position = StartsFromZero.RestartAtZero(roll, Position);
-                }
-                else if (Position == 95 && roll <= 5)
-                {
-                    Position = StartsFromZero.RestartAtZero(roll, Position);
-                }
-                else if (Position < 95)
-                {
-                    Position = StartsFromZero.RestartAtZero(roll, Position);
-                }
-                Counter++;
-                Console.WriteLine("Num of Dice Roll is {0} & Player positio is {1} " , Counter, Position);
+
+            //int roll = DiceRoll.DiceRollValue();
+            Console.WriteLine("Dice roll value is " + roll);
+            if (Position == 99 && roll == 1)
+            {
+                Position = StartsFromZero.RestartAtZero(roll, Position);
             }
-        
-            Console.WriteLine("Players Final Position " + Position + " Number of total dice roll to reach winning Position " + Counter);
-        }
+            else if (Position == 98 && roll <= 2)
+            {
+                Position = StartsFromZero.RestartAtZero(roll, Position);
+
+            }
+            else if (Position == 97 && roll <= 3)
+            {
+                Position = StartsFromZero.RestartAtZero(roll, Position);
+            }
+            else if (Position == 96 && roll <= 4)
+            {
+                Position = StartsFromZero.RestartAtZero(roll, Position);
+            }
+            else if (Position == 95 && roll <= 5)
+            {
+                Position = StartsFromZero.RestartAtZero(roll, Position);
+            }
+            else if (Position < 95)
+            {
+                Position = StartsFromZero.RestartAtZero(roll, Position);
+            }
+           
+                return Position;
+            
+        }   
     }
 }
