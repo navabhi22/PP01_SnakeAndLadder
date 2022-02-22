f34703a1f44fec4d717a392269d7d63a6c186434
navabhi22 <navabhi22@gmail.com>
2022-02-22 12:51:07 +0530

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace PP01SnakeAndLadder
             Console.WriteLine("This problem simulates a Snake and Ladder Game");
             int StartPositon = StartPosition.StartPositionInitialisation();
             Console.WriteLine("Start Position " + StartPositon);
+            int roll = DiceRoll.DiceRollValue();
+            Console.WriteLine("value of Dice Roll " + roll);
         }
     }
 }
