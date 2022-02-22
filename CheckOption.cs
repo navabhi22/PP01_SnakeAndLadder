3b8c95db899f47ced869b7b31820fbafcc86af79
navabhi22 <navabhi22@gmail.com>
2022-02-22 17:58:47 +0530

    Updated class to Play again for ladder

--- CheckOption.cs
+++ CheckOption.cs
@@ -9,6 +9,7 @@ namespace PP01SnakeAndLadder
     public class CheckOption
     {
         /// <summary>
+        /// Updated for Play again at Ladder
         /// Takes input roll value and Position.
         /// Generate Options Snake, Ladder and NoPlay and Changes Postion accordingly.
         /// Return the Position
@@ -24,18 +25,25 @@ namespace PP01SnakeAndLadder
             int option = random.Next(0, 3);
             if (option == Snake)
             {
-                Position -= roll;
                 Console.WriteLine("option - snake");
+                Position -= roll;
+                
             }
             else if (option == Ladder)
             {
-                Position += roll;
                 Console.WriteLine("option -Ladder");
+                Position += roll;
+                //Console.WriteLine("player" + Position);
+                //int roll1 = DiceRoll.DiceRollValue();
+                //Position = ExactWin.ExactWinningPossition(Position, roll1);
+                //Position = Option(roll1, Position);
+
             }
             else
             {
-                Position += 0;
                 Console.WriteLine("option -NoPlay");
+                Position += 0;
+                
             }
             return Position;
         }
