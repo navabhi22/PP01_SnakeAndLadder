c8b59f1cee31224b93df1bce16fc823439746187
navabhi22 <navabhi22@gmail.com>
2022-02-22 12:42:05 +0530

    Added class for Start Position Initialisation

---
+++ StartPosition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP01SnakeAndLadder
+{
+    public class StartPosition
+    {
+        /// <summary>
+        /// Start Position initialisation.
+        /// Returns Start Position.
+        /// </summary>
+        /// <returns></returns>
+        public static int StartPositionInitialisation()
+        {
+            int StartPosition = 0;
+            return StartPosition;
+        }
+    }
+}
